ef0b88dccfe0491e6c96ed8b73e338ab474cc3f1
Shaurya Agarwal <114003409+APUNJIA@users.noreply.github.com>
2025-02-09 04:54:18 +0530

    Updated views.py

--- ELK/views.py
+++ ELK/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import logging
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+logger = logging.getLogger('django')
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        logger.info(f"Protected view accessed by user: {request.user.username}")
+        return Response({
+            "message": "You have access to this protected view",
+            "user": request.user.username
+        })
 
 # Create your views here.
